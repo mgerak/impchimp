2b18205aa102d1b1500635a63a93d6917c062fb0
Michael <michael@michael-VirtualBox.(none)>
2013-11-24 06:26:58 -0500

    xxx

---
+++ db/migrate/20131124110313_alter_pizzas_fix_latlong.rb
@@ -0,0 +1,6 @@
+class AlterPizzasFixLatlong < ActiveRecord::Migration
+  def change
+  	change_column :pizzas, :lat, :decimal, :precision => 15, :scale => 10
+  	change_column :pizzas, :lat, :decimal, :precision => 15, :scale => 10
+  end
+end
